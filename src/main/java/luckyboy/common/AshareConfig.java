8b6343010c466ccb180b44f8262f01b9180113dd
putin <735140144@qq.com>
2023-03-26 21:40:16 +0800

    推送github准备

--- src/main/java/luckyboy/common/AshareConfig.java
+++ src/main/java/luckyboy/common/AshareConfig.java
@@ -2,13 +2,13 @@ package luckyboy.common;
 
 /**
  * @author
- * @Description
+ * @Description pushdeer推送
  * @date 2022-06-23
  */
 
 public class AshareConfig {
-    public static final String PushUrl = "https://sctapi.ftqq.com/SCT183946TmQ6u7XxA1s1cdYVC7wrMiiZo.send";
-    public static final String PushDeerSelf ="http://120.48.27.119:8800/";
-    public static final String PushKey = "PDU1TTHCpVywlgFcAIKObwQo0kli5cjK41rH2";
+    public static final String PushUrl = "";
+    public static final String PushDeerSelf ="";
+    public static final String PushKey = "";
 
 }
